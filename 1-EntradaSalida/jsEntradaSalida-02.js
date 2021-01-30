8cb5b138f4c6e133ea761fb3966cfe017a60ebd2
edgarluna04 <edgarluna16@hotmail.com>
2021-01-30 20:30:24 -0300

    cambios sabado

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -3,7 +3,10 @@ Debemos lograr tomar un nombre con 'prompt'
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	let nombre = prompt("ingrese su nombre");
+	let nombre = prompt("ingrese nombre"); //declaro la variable nombre y le asigno el valor despues del =
+											//si no se ingresa ningun valor se devuelve una cadena vacia (string)
+	
+	
 	alert(nombre);
 }
 
