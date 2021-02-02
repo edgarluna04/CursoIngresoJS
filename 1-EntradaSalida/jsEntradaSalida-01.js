65ac0d11fa123cef825dbfda9fcdc1a1c43eaaed
edgarluna04 <edgarluna16@hotmail.com>
2021-02-01 21:50:55 -0300

    clase lunes 1 de feb 2021

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -6,3 +6,5 @@ function mostrar()
 	alert("Esto funciona de maravilla");
 }
 
+
+
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,5 +1,13 @@
 
 function mostrar()
 {
+    let dato1;
+    let dato2;
 
+    dato1 = prompt("ingrese ancho");
+    dato2 = prompt("ingrese largo");
+
+    resultado = parseFloat(dato1) + parseFloat(dato2);
+
+    alert(resultado * 2)
 }
